a97247a212d1cad0a33d827ff0678fdc6b01bfa3
zaz <zaz@home.zakz.biz>
2025-10-31 14:45:38 -0400

    adding pdf of md presentation

---
+++ port-forwarding/script.sh
@@ -0,0 +1,7 @@
+md2pdf -i presentation.md \
+    -o presentation.pdf \
+    --theme white \
+    --new-page-on-hr \
+    --with-footer \
+    --author "Zak Zebrowski <zak.zebrowski@gmail.com>" \
+    --title "SSH Port Forwarding"
