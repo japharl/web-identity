324e4a957b7cd502a549c071256ddbde66251a7c
zaz <zaz@home.zakz.biz>
2025-10-31 14:50:38 -0400

    updating options.

--- port-forwarding/script.sh
+++ port-forwarding/script.sh
@@ -1,7 +1,6 @@
 md2pdf -i presentation.md \
     -o presentation.pdf \
     --theme white \
-    --new-page-on-hr \
     --with-footer \
     --author "Zak Zebrowski <zak.zebrowski@gmail.com>" \
     --title "SSH Port Forwarding"
